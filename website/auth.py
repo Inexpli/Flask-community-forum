e555473e839d9bb67555ab42f8e8bebd5f3713ea
INEXPLI <61479966+JezJakub@users.noreply.github.com>
2021-03-14 23:46:36 +0100

    Login, database.

--- website/auth.py
+++ website/auth.py
@@ -1,17 +1,31 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET','POST'])
 def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
 
+        user_username = User.query.filter_by(username=username).first()
+
+        if user_username:
+            if check_password_hash(user_username.password, password):
+                flash('Logged in successfuly.', category='success')
+                return redirect(url_for('views.index'))
+            else:
+                flash('Password is incorrect.', category='error')
+        else:
+            flash('User doesn\'t exist', category='error')
     return render_template('login.html')
 
 @auth.route('/logout')
 def logout():
-
-    pass
+    return redirect(url_for('auth.login'))
 
 @auth.route('/signup', methods=['GET','POST'])
 def singup():
@@ -20,12 +34,20 @@ def singup():
         email = request.form.get('email')
         password = request.form.get('password')
         confirm_password = request.form.get('confirm_password')
-        if len(username) < 3 or len(username)>20:
+
+        user_username = User.query.filter_by(username=username).first()
+
+        if user_username:
+            flash("Username already exist", category='error')
+        elif len(username) < 3 or len(username)>20:
             flash("Username must be greater than 3 characters and lower than 20.", category='error')
         elif len(email) < 4:
             flash("Email must be greater than 4 characters.", category='error')
         elif password != confirm_password:
             flash("Passwords don\'t match.", category='error')
         else:
+            new_user = User(username=username, email=email, password=generate_password_hash(password, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
             flash("Account has been created!", category='success')
     return render_template('signup.html')
--- website/models.py
+++ website/models.py
@@ -4,14 +4,14 @@ from sqlalchemy.sql import func
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    nickname = db.Column(db.String(20))
+    username = db.Column(db.String(20))
     data = db.Column(db.String(10000))
     date = db.Column(db.DateTime(timezone=True), default=func.now())
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    nickname = db.Column(db.String(20), unique=True)
+    username = db.Column(db.String(20), unique=True)
     email = db.Column(db.String(50), unique=True)
     password = db.Column(db.String(150))
     notes = db.relationship('Post')
