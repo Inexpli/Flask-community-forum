74671dd696c2ab860e3a9f859ab0cf0c93603d31
INEXPLI <61479966+JezJakub@users.noreply.github.com>
2021-03-14 01:33:43 +0100

    Database, database models, sign up form

--- website/__init__.py
+++ website/__init__.py
@@ -1,14 +1,29 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+
+db = SQLAlchemy()
+
+DB_NAME = 'database.db'
 
 def create_app():
     #Config
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'secretkey'
-    
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
     #Routes
     from .views import views
     app.register_blueprint(views, url_prefix='/')
     from .auth import auth
     app.register_blueprint(auth, url_prefix='/')
+    #Database
+    create_database(app)
 
     return app
+
+
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
+        db.create_all(app=app)
+        print('Database has been created!')
--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
 
 auth = Blueprint('auth', __name__)
 
@@ -14,5 +15,17 @@ def logout():
 
 @auth.route('/signup', methods=['GET','POST'])
 def singup():
-
+    if request.method == 'POST':
+        username = request.form.get('username')
+        email = request.form.get('email')
+        password = request.form.get('password')
+        confirm_password = request.form.get('confirm_password')
+        if len(username) < 3 or len(username)>20:
+            flash("Username must be greater than 3 characters and lower than 20.", category='error')
+        elif len(email) < 4:
+            flash("Email must be greater than 4 characters.", category='error')
+        elif password != confirm_password:
+            flash("Passwords don\'t match.", category='error')
+        else:
+            flash("Account has been created!", category='success')
     return render_template('signup.html')
