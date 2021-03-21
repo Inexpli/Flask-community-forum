8e391146858b634ce5ea72ab92cbd96861e07a93
INEXPLI <61479966+JezJakub@users.noreply.github.com>
2021-03-21 18:58:52 +0100

    conf

--- website/__init__.py
+++ website/__init__.py
@@ -13,14 +13,6 @@ def create_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
 
-    login_manager = LoginManager()
-    login_manager.login_view = 'auth.login'
-    login_manager.init_app(app)
-
-    @login_manager.user_loader
-    def load_user(id):
-        return User.query.get(int(id))
-
     from .views import views
     from .auth import auth
 
@@ -30,6 +22,13 @@ def create_app():
     from .models import User
 
     create_database(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
 
     return app
 
