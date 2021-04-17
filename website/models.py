3441f46a2f0c0c9242116a0e2c460b171abe801c
Inexpli <61479966+Inexpli@users.noreply.github.com>
2021-04-17 16:39:16 +0200

    17.04 16:40

--- website/models.py
+++ website/models.py
@@ -3,9 +3,8 @@ from flask_login import UserMixin
 from sqlalchemy.sql import func
 
 
-class Post(db.Model):
+class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.username'))
     data = db.Column(db.String(10000))
     date = db.Column(db.DateTime(timezone=True), default=func.now())
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
@@ -15,4 +14,4 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(20), unique=True)
     email = db.Column(db.String(50), unique=True)
     password = db.Column(db.String(150))
-    notes = db.relationship('Post')
+    notes = db.relationship('Note')
--- website/views.py
+++ website/views.py
@@ -1,14 +1,22 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 from flask_login import login_required, login_user, logout_user, current_user
+from .models import Note
+from . import db
 
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def index():
-    return render_template('index.html', user = current_user)
+    if request.method == 'POST':
+        note = request.form.get('note')
 
-@views.route('/home')
-@login_required
-def home():
-    return render_template('home.html')
+        if len(note) < 1:
+            flash('Note is too short!', category='error')
+        else:
+            new_note = Note(data=note, user_id=current_user.id)
+            db.session.add(new_note)
+            db.session.commit()
+            flash('Note added!', category='success')
+
+    return render_template("index.html", user=current_user)
