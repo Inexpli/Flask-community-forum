003f526bc4295f16632eaa7cb11d68320c9f4f24
Inexpli <61479966+Inexpli@users.noreply.github.com>
2021-04-23 23:38:29 +0200

    patches

--- website/views.py
+++ website/views.py
@@ -25,5 +25,6 @@ def index():
     return render_template("index.html", user=current_user)
 
 @views.route('/profile')
+@login_required
 def profile():
     return render_template("profile.html", user=current_user)
