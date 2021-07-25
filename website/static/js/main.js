05fbca589c0f79987f8ecca6accdb2ff802317e8
Inexpli <61479966+Inexpli@users.noreply.github.com>
2021-07-25 19:14:03 +0200

    Update main.js

--- website/static/js/main.js
+++ website/static/js/main.js
@@ -2,15 +2,6 @@ function editButton() {
     console.log('działam')
 };
 
-// // function deleteNote(noteId) {
-// //     fetch("/delete-note", {
-// //       method: "POST",
-// //       body: JSON.stringify({ noteId: noteId }),
-// //     }).then((_res) => {
-// //       window.location.href = "/";
-// //     });
-// // }
-
 function showEdit(noteId) {
   var title = document.getElementById('titleEdit')
   var titleContent = document.getElementById(`noteTitleValue${noteId}`).innerText
@@ -44,17 +35,3 @@ function deleteNote(noteId) {
     window.location.href = "/";
   });
 }
-
-
-
-// function editButton() {
-//     console.log('Stage 1')
-//     var button = document.getElementById('submitEdit')
-//     var titleEdit = document.getElementById('titleEdit')
-//     // var titleEditValue = target.closest('noteTitleValue').innerText
-//     var titleEditValue = document.getElementById('noteTitleValue').innerText
-//     var noteEdit = document.getElementById('noteEdit')
-//     var noteEditValue = document.getElementById('noteValue').innerText
-//     titleEdit.value = titleEditValue
-//     noteEdit.value = noteEditValue
-// }
